ab512345b9732954e505621f4d6b23767372245e
itsAhtesham <ahteshamsiddiqui5900@gmail.com>
2025-03-15 08:18:43 +0530

    Added message card.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,35 @@
 "use client";
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+import { User } from "next-auth";
+import { signOut, useSession } from "next-auth/react";
+import { Button } from "./ui/button";
+import Link from "next/link";
 
-export default Navbar
+const Navbar = () => {
+  const { data: session } = useSession();
+  const user: User = session?.user as User;
+
+  return (
+    <nav className="p-4 md:p-6 shadow-md">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <a href="#">Mystery Message</a>
+        {session ? (
+          <>
+            <span className="mr-4">
+              Welcome, {user?.username || user?.email}
+            </span>
+            <Button className="w-full md:w-auto" onClick={() => signOut()}>
+              Logout
+            </Button>
+          </>
+        ) : (
+          <Link href={"/sign-in"}>
+            <Button>Login</Button>
+          </Link>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
